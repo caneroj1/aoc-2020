3489210a25f001aa61d5e5ab0c7c374f9f237d18
Joseph Canero <jmc41493@gmail.com>
2020-12-03 20:44:58 -0500

    refactor

--- src/main/scala/jcanero/day-three/DayThree.scala
+++ src/main/scala/jcanero/day-three/DayThree.scala
@@ -41,6 +41,8 @@ object DayThreePartOne {
 }
 
 object DayThreePartTwo {
+  import DayThreeHelpers._
+
   case class Position(row: Int, col: Int)
   case class Slope(
       position: Option[Position],
@@ -50,11 +52,32 @@ object DayThreePartTwo {
     def canAdvance(): Boolean = !position.isEmpty
   }
 
-  // 31696434 wrong
-  import DayThreeHelpers._
+  def advanceSlope(map: Vector[Vector[Tile]])(slope: Slope): Slope = {
+    slope.position match {
+      case None => slope
+      case Some(Position(row, col)) => {
+        val currentRow = map(row)
+
+        val inc = currentRow(col) match {
+          case Tree  => 1
+          case Empty => 0
+        }
+
+        val newCount = slope.treeCount + inc
+        val newRow = row + slope.increment.row
+        if (newRow < map.length) {
+          val newCol = (col + slope.increment.col) % currentRow.length
+          Slope(Some(Position(newRow, newCol)), slope.increment, newCount)
+        } else {
+          Slope(None, slope.increment, newCount)
+        }
+      }
+    }
+  }
+
   def main(args: Array[String]) = {
     val map = getMap(args)
-    val slopes = Array(
+    var slopes = Array(
       Slope(Some(Position(0, 0)), Position(1, 1), 0),
       Slope(Some(Position(0, 0)), Position(1, 3), 0),
       Slope(Some(Position(0, 0)), Position(1, 5), 0),
@@ -62,29 +85,12 @@ object DayThreePartTwo {
       Slope(Some(Position(0, 0)), Position(2, 1), 0)
     )
 
-    while (slopes.exists(_.canAdvance())) {
-      for (i <- Range(0, slopes.length)) {
-        val slope = slopes(i)
-        slope.position match {
-          case None => {}
-          case Some(Position(row, col)) => {
-            val currentRow = map(row)
-
-            val inc = currentRow(col) match {
-              case Tree  => 1
-              case Empty => 0
-            }
-
-            val newCount = slope.treeCount + inc
-            val newRow = row + slope.increment.row
-            if (newRow < map.length) {
-              val newCol = (col + slope.increment.col) % currentRow.length
-              slopes(i) =
-                Slope(Some(Position(newRow, newCol)), slope.increment, newCount)
-            } else {
-              slopes(i) = Slope(None, slope.increment, newCount)
-            }
-          }
+    var stillSlopes = true
+    while (stillSlopes) {
+      slopes.partition(_.canAdvance()) match {
+        case (Array(), _) => stillSlopes = false
+        case (validSlopes, invalidSlopes) => {
+          slopes = validSlopes.map(advanceSlope(map)).concat(invalidSlopes)
         }
       }
     }
